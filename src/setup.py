2bc38ca429e2a64acc171eebccd25065bf4d57e4
StpME <54916725+StpME@users.noreply.github.com>
2025-02-10 11:34:27 -0800

    Adjust backdrop adder positioning

--- src/setup.py
+++ src/setup.py
@@ -26,15 +26,17 @@ class Setup:
         self.backdrop_menu_label = tk.Label(self.root, text="", font=("Arial", 12))
         self.backdrop_menu_label.pack(side=tk.LEFT)
 
-        self.backdrop_entry = tk.Entry(self.root, width=50)
-        self.backdrop_entry.pack(side=tk.RIGHT, padx=5, pady=5)
+        # Backdrop frame for button and entry box
+        self.bottom_frame = tk.Frame(self.root)
+        self.bottom_frame.pack(side=tk.BOTTOM, anchor=tk.SW, fill=tk.X, padx=5, pady=5)
 
         # Add backdrop button
-        self.add_backdrop_btn = tk.Button(self.root, text="Add Backdrop", 
+        self.add_backdrop_btn = tk.Button(self.bottom_frame, text="Add Backdrop", 
                                         command=self.add_backdrop_to_css,
                                         bd=3 # Border width
                                     )
-        self.add_backdrop_btn.pack(side=tk.RIGHT,
-                                padx=5,
-                                pady=5,
-                            )
+        self.add_backdrop_btn.pack(side=tk.LEFT, padx=5, pady=5)
+        
+        # Backdrop entry/input box
+        self.backdrop_entry = tk.Entry(self.bottom_frame, width=50)
+        self.backdrop_entry.pack(side=tk.LEFT, padx=5, pady=5, expand=True, fill=tk.X)
