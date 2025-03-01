8ec71c3fc912acd33dccdf34b45f597a3ebd516a
StpME <54916725+StpME@users.noreply.github.com>
2025-03-01 03:59:33 -0800

    lint + docstr setup & backdrop manager

--- src/backdrop_manager.py
+++ src/backdrop_manager.py
@@ -1,8 +1,26 @@
 class BackdropManager:
+    """
+    Manages the modification of CSS files to update and check active backdrops.
+    """
+
     def __init__(self, css_file_path):
+        """
+        Initialize the BackdropManager with the path to the CSS file.
+
+        Args:
+            css_file_path (string): The path to the CSS file.
+        """
         self.css_file_path = css_file_path
 
     def update_css_file(self, active_backdrop, bg_str):
+        """
+        Update the CSS file to select or deselect backdrops.
+
+        Args:
+            active_backdrop (string): The selected backdrop URL.
+            bg_str (string): The theme specific CSS text used
+            to identify backdrop lines, set by the theme config.
+        """
         if active_backdrop:
             with open(self.css_file_path, "r") as file:
                 lines = file.readlines()
@@ -13,7 +31,8 @@ class BackdropManager:
                         if active_backdrop in line:
                             # Uncomment only active backdrop
                             if self.is_backdrop_commented(line):
-                                file.write(line.replace("/*", "").replace("*/", ""))
+                                file.write(line.replace("/*", "")
+                                           .replace("*/", ""))
                             else:
                                 file.write(line)
                         else:
@@ -26,4 +45,13 @@ class BackdropManager:
                         file.write(line)
 
     def is_backdrop_commented(self, line):
+        """
+        Check if the given backdrop line in the file is commented out.
+
+        Args:
+            line (string): The line from the CSS file to check.
+
+        Returns:
+            (boolean): True if the line is commented out, False otherwise.
+        """
         return line.strip().startswith("/*") and line.strip().endswith("*/")
--- src/setup.py
+++ src/setup.py
@@ -1,68 +1,80 @@
 import tkinter as tk
 import webbrowser
+
+
 class Setup:
+    """
+    Handles the setup and configuration of the GUI,
+    including program layout and event key bindings.
+    """
     REPO = "StpME/VCThemeGUI"
     EXE_NAME = "VCTheme.exe"
 
     def setup_gui(self, gui_name):
+        """
+        Set up the GUI layout and components.
+
+        Args:
+            gui_name (string): The name of the GUI theme being set up.
+        """
         # Create an dark border for labels
         dark_frame = tk.Frame(self.root, bg="gray")
         dark_frame.pack(fill="x")
 
         # Create instruction header label frame
-        label_frame = tk.Frame(dark_frame, bg="lightgray") 
-        label_frame.pack(fill="x", pady=2) 
+        label_frame = tk.Frame(dark_frame, bg="lightgray")
+        label_frame.pack(fill="x", pady=2)
 
-        label = tk.Label(label_frame, text=("Select " + gui_name + " theme from File menu"), font=("Arial", 12), bg="lightgray")
+        label = tk.Label(label_frame, text=("Select " + gui_name +
+                         " theme from File menu"),
+                         font=("Arial", 12), bg="lightgray")
         label.pack()
 
         # Text shown for main window
         self.text = tk.Text(self.root, wrap="word")
         self.text.pack(expand=True, fill="both")
 
-
-
-
         # Backdrop frame for button and entry box
-        btm_frame = tk.Frame(self.root, bg="lightgray",  highlightbackground="gray", highlightthickness=2)
-        btm_frame.pack(side="bottom", fill="x") 
+        btm_frame = tk.Frame(self.root, bg="lightgray",
+                             highlightbackground="gray", highlightthickness=2)
+        btm_frame.pack(side="bottom", fill="x")
 
         # Dropdown menu - Backdrop selector
         self.backdrop_options = tk.StringVar(value="Select Backdrop")
-        self.backdrop_menu = tk.OptionMenu(btm_frame, self.backdrop_options,"Select Below")
+        self.backdrop_menu = tk.OptionMenu(btm_frame, self.backdrop_options,
+                                           "Select Below")
         self.backdrop_menu.pack(side="left")
         self.backdrop_menu['menu'].entryconfig(0, state="disabled")
         self.backdrop_menu['menu'].add_separator()
 
-        # Add backdrop button
-        self.add_backdrop_btn = tk.Button(btm_frame, text="Add Backdrop", command=self.add_backdrop_to_css, bd=3)
+        self.add_backdrop_btn = tk.Button(btm_frame, text="Add Backdrop",
+                                          command=self.add_backdrop_to_css,
+                                          bd=3)
         self.add_backdrop_btn.pack(side="left", padx=5, pady=5)
 
-        # Backdrop entry/input box
         self.backdrop_entry = tk.Entry(btm_frame)
         self.backdrop_entry.pack(side="left", padx=5, pady=5)
-
-
-
-
-
 
         # Calculate and set the entry box width to 75% of the window width
         def set_entry_width(event=None):
             window_width = self.root.winfo_width()
             entry_width = int(window_width * 0.75) // 9
-            self.backdrop_entry.config(width = entry_width)
+            self.backdrop_entry.config(width=entry_width)
 
         # Bind the function to window resize event for dynamic adjustments
         self.root.bind("<Configure>", set_entry_width)
 
         set_entry_width()
-        
-    # Toggle window stay on top
+
     @staticmethod
     def toggle_stay_on_top(self):
+        """
+        Toggle whether the program window stays on top of other windows.
+        """
         self.attributes("-topmost", not self.attributes("-topmost"))
 
-    # Link to project Github page
     def open_github():
+        """
+        Open the project's Github page in user's default web browser.
+        """
         webbrowser.open_new("https://github.com/StpME/VCThemeGUI")
