5b2e94fe10076826f3d2edad621a9260700f6050
StpME <54916725+StpME@users.noreply.github.com>
2025-03-01 05:09:00 -0800

    lint + docstr dscplus & softx

--- src/dscplus.py
+++ src/dscplus.py
@@ -7,41 +7,57 @@ from backdrop_manager import BackdropManager
 from file_manager import FileManager
 from base_gui import BaseGUI
 
+
 class DSCPlusGUI(BaseGUI):
+    """
+    Theme specific class for individual syntax not managed by the base gui.
+    Includes methods for extracting and adding backdrops to the CSS file.
+    Inherits from the BaseGUI class.
+    """
     def __init__(self, root):
-        # Theme configuration
+        """
+        Initialize the Discord+ theme GUI with the root window,
+        and relevant helper class instances.
+
+        Args:
+            root (tk.Tk): The root Tkinter window.
+        """
         self.theme_config = ["Discord+", "DiscordPlus", "--dplus-backdrop"]
 
         self.username = os.getlogin()
         self.css_file_path = None
 
-        # Initialize file and backdrop managers
         self.file_manager = FileManager(self.theme_config)
         self.backdrop_manager = BackdropManager(None)
 
         # Initialize base class template
-        super().__init__(root, self.theme_config, self.file_manager, self.backdrop_manager)
+        super().__init__(root, self.theme_config,
+                         self.file_manager, self.backdrop_manager)
 
-        # Set up the GUI using the Setup class
         self.setup = Setup
         self.setup.setup_gui(self, self.theme_config[0])
 
-        # Remove the existing text widget
+        # Remove any existing text widget
         self.text.destroy()
 
         # Create a new frame for the image grid
         self.img_grid_frame = tk.Frame(self.root)
         self.img_grid_frame.pack(fill="both", expand=True)
 
-        # Initialize image preview instance
         self.image_preview_instance = None
-        self.root.title(f"VCTheme | {self.theme_config[0]} | {self.current_version}")
 
-        # Set up the menu
+        self.root.title(
+            f"VCTheme | {self.theme_config[0]} | {self.current_version}")
+
         self.setup_menu()
 
-    # Extract existing backdrops from file
     def extract_backdrops(self, css_text):
+        """
+        Extract existing backdrop urls from the CSS file.
+
+        Args:
+            css_text (string): The CSS file content.
+        """
         backdrop_urls_dark = []
         backdrop_urls_light = []
         lines = css_text.split("\n")
@@ -58,19 +74,28 @@ class DSCPlusGUI(BaseGUI):
                 elif backdrop_section == "light":
                     backdrop_urls_light.append(backdrop_url)
         return backdrop_urls_dark + backdrop_urls_light
-    
-    # Override base method to allow for backdrop manager initialization
+
     def open_file(self):
+        """
+        Open a file dialog to select a CSS file.
+        Overrides the base method to initialize the BackdropManager.
+        """
         file_path = super().open_file()
         if file_path:
             self.css_file_path = file_path
             self.backdrop_manager = BackdropManager(self.css_file_path)
 
-    # Add a new backdrop URL to the CSS file
     def add_backdrop_to_css(self):
-        # First check if a file has been loaded
-        if self.css_file_path is None or not os.path.exists(self.css_file_path):
-            messagebox.showerror("Error", "Please load a CSS file before adding a backdrop.")
+        """
+        Add a new backdrop url to the CSS file.
+        Includes checks for loaded file, duplicates and valid url formats.
+        """
+        # Check if a file has been loaded
+        if (self.css_file_path is None or
+                not os.path.exists(self.css_file_path)):
+            messagebox.showerror(
+                "Error",
+                "Please load a CSS file before adding a backdrop.")
             return
         link = self.backdrop_entry.get()
         if link and re.match(r'^https?:\/\/.*\.(png|jpg|jpeg|gif)$', link):
@@ -89,27 +114,42 @@ class DSCPlusGUI(BaseGUI):
                 index_last_backdrop_dark = -1
                 index_last_backdrop_light = -1
                 for i, line in reversed(list(enumerate(css_content))):
-                    if self.theme_config[2] in line and i > backdrop_section_dark and (backdrop_section_light == -1 or i < backdrop_section_light):
+                    if (self.theme_config[2] in line and
+                            i > backdrop_section_dark and
+                            (backdrop_section_light == -1 or
+                             i < backdrop_section_light)):
                         index_last_backdrop_dark = i
                         break
 
                 for i, line in reversed(list(enumerate(css_content))):
-                    if self.theme_config[2] in line and i > backdrop_section_light:
+                    if (self.theme_config[2] in line and
+                            i > backdrop_section_light):
                         index_last_backdrop_light = i
                         break
 
                 with open(self.css_file_path, "w") as file:
                     for i, line in enumerate(css_content):
                         file.write(line)
-                        if i == index_last_backdrop_dark and backdrop_section_dark != -1:
-                            file.write(f"/*{self.theme_config[2]}: url({link});*/\n")
-                        if i == index_last_backdrop_light and backdrop_section_light != -1:
-                            file.write(f"/*{self.theme_config[2]}: url({link});*/\n")
+                        if (i == index_last_backdrop_dark and
+                                backdrop_section_dark != -1):
+                            file.write(
+                                f"/*{self.theme_config[2]}: url({link});*/\n")
+                        if (i == index_last_backdrop_light and
+                                backdrop_section_light != -1):
+                            file.write(
+                                f"/*{self.theme_config[2]}: url({link});*/\n")
 
-                self.backdrop_menu['menu'].add_command(label=link, command=lambda u=link: self.set_active_backdrop(u))
+                self.backdrop_menu['menu'].add_command(
+                    label=link,
+                    command=lambda u=link: self.set_active_backdrop(u))
                 self.backdrop_entry.delete(0, tk.END)
                 self.update_image_previews()
-            else:
-                messagebox.showerror("Error", "This link is already present in the list of backdrops.")
-        else:
-            messagebox.showerror("Error", "Please enter a valid URL with a .png, .jpg/jpeg, or .gif extension.")
+            else:  # Check if the link is already present in the file
+                messagebox.showerror(
+                    "Error",
+                    "This link is already present in the list of backdrops.")
+        else:  # Check if given valid url format
+            messagebox.showerror(
+                "Error",
+                "Please enter a valid URL with a "
+                ".png, .jpg/jpeg, or .gif extension.")
--- src/softx.py
+++ src/softx.py
@@ -7,41 +7,57 @@ from backdrop_manager import BackdropManager
 from file_manager import FileManager
 from base_gui import BaseGUI
 
+
 class SoftXGUI(BaseGUI):
+    """
+    Theme specific class for individual syntax not managed by the base gui.
+    Includes methods for extracting and adding backdrops to the CSS file.
+    Inherits from the BaseGUI class.
+    """
     def __init__(self, root):
-        # Theme configuration
+        """
+        Initialize the SoftX theme GUI with the root window,
+        and relevant helper class instances.
+
+        Args:
+            root (tk.Tk): The root Tkinter window.
+        """
         self.theme_config = ["SoftX", "SoftX", "--background-image"]
 
         self.username = os.getlogin()
         self.css_file_path = None
 
-        # Initialize file and backdrop managers
         self.file_manager = FileManager(self.theme_config)
         self.backdrop_manager = BackdropManager(None)
 
         # Initialize base class template
-        super().__init__(root, self.theme_config, self.file_manager, self.backdrop_manager)
+        super().__init__(root, self.theme_config,
+                         self.file_manager, self.backdrop_manager)
 
-        # Set up the GUI using the Setup class
         self.setup = Setup
         self.setup.setup_gui(self, self.theme_config[0])
 
-        # Remove the existing text widget
+        # Remove any existing text widget
         self.text.destroy()
 
         # Create a new frame for the image grid
         self.img_grid_frame = tk.Frame(self.root)
         self.img_grid_frame.pack(fill="both", expand=True)
 
-        # Initialize image preview instance
         self.image_preview_instance = None
-        self.root.title(f"VCTheme | {self.theme_config[0]} | {self.current_version}")
 
-        # Set up the menu
+        self.root.title(
+            f"VCTheme | {self.theme_config[0]} | {self.current_version}")
+
         self.setup_menu()
 
-    # Extract existing backdrops from file
     def extract_backdrops(self, css_text):
+        """
+        Extract existing backdrop urls from the CSS file.
+
+        Args:
+            css_text (string): The CSS file content.
+        """
         backdrop_urls = []
         lines = css_text.split("\n")
         for line in lines:
@@ -50,19 +66,28 @@ class SoftXGUI(BaseGUI):
                 if backdrop_url not in backdrop_urls:
                     backdrop_urls.append(backdrop_url)
         return backdrop_urls
-    
-    # Override base method to allow for backdrop manager initialization
+
     def open_file(self):
+        """
+        Open a file dialog to select a CSS file.
+        Overrides the base method to initialize the BackdropManager.
+        """
         file_path = super().open_file()
         if file_path:
             self.css_file_path = file_path
             self.backdrop_manager = BackdropManager(self.css_file_path)
 
-    # Add a new backdrop URL to the CSS file
     def add_backdrop_to_css(self):
-        # First check if a file has been loaded
-        if self.css_file_path is None or not os.path.exists(self.css_file_path):
-            messagebox.showerror("Error", "Please load a CSS file before adding a backdrop.")
+        """
+        Add a new backdrop url to the CSS file.
+        Includes checks for loaded file, duplicates and valid url formats.
+        """
+        # Check if a file has been loaded
+        if (self.css_file_path is None or
+                not os.path.exists(self.css_file_path)):
+            messagebox.showerror(
+                "Error",
+                "Please load a CSS file before adding a backdrop.")
             return
         link = self.backdrop_entry.get()
         if link and re.match(r'^https?:\/\/.*\.(png|jpg|jpeg|gif)$', link):
@@ -71,7 +96,9 @@ class SoftXGUI(BaseGUI):
 
             # Check if the link is already present in the file
             if any(f"url({link})" in line for line in css_content):
-                messagebox.showerror("Error", "This link is already present in the list of backdrops.")
+                messagebox.showerror(
+                    "Error",
+                    "This link is already present in the list of backdrops.")
                 return
 
             # Find the index where the new backdrop should be added
@@ -79,7 +106,7 @@ class SoftXGUI(BaseGUI):
             for i, line in enumerate(css_content):
                 if self.theme_config[2] in line and "url(" in line:
                     backdrop_index = i
-                # bg blur comment fencepost fix
+                # specific bg blur file comment fix
                 elif line.strip().startswith("--background-blur"):
                     break
 
@@ -87,10 +114,16 @@ class SoftXGUI(BaseGUI):
                 for i, line in enumerate(css_content):
                     file.write(line)
                     if i == backdrop_index:
-                        file.write(f"/*{self.theme_config[2]}: url({link});*/\n")
+                        file.write(
+                            f"/*{self.theme_config[2]}: url({link});*/\n")
 
-            self.backdrop_menu['menu'].add_command(label=link, command=lambda u=link: self.set_active_backdrop(u))
+            self.backdrop_menu['menu'].add_command(
+                label=link,
+                command=lambda u=link: self.set_active_backdrop(u))
             self.backdrop_entry.delete(0, tk.END)
             self.update_image_previews()
-        else:
-            messagebox.showerror("Error", "Please enter a valid URL with a .png, .jpg/jpeg, or .gif extension.")
+        else:  # Check if given valid url format
+            messagebox.showerror(
+                "Error",
+                "Please enter a valid URL with a "
+                ".png, .jpg/jpeg, or .gif extension.")
